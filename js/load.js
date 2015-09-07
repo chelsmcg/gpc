0938347eb331d0ece7765228bb030fca48418881
Chelsea <chelsea@sindesign.com.au>
2015-09-07 21:37:28 +1000

    styling and edit and discovery page

--- js/load.js
+++ js/load.js
@@ -7,6 +7,10 @@ var Load = {
 
 	events: function() {
 		$('#mainContainer').on('click touch', '#addPackageBtn', Load.addPackage);
+		$('body').on('click touch', '.companyLogo', Load.dashboard);
+		$('body').on('click touch', '#editIcon', Load.addEditPackage);
+		$('body').on('click touch', '#discoveryBtn', Load.discoveryPage);
+		
 	},
 
 	dashboard: function() {
@@ -15,6 +19,28 @@ var Load = {
 
 	addPackage: function() {
 		$('#mainContainer').load('components.html #addPackage');
+	},
+
+	addEditPackage: function(){
+		$('#mainContainer').load('components.html #addPackage', function(){
+			$('.dashTitle').text('EDIT PACKAGE - PACKAGE NAME');
+			$('#pageIcon').text('M');
+			$('.formHeader').text('EDIT PACKAGE');
+		});
+	},
+
+	discoveryPage: function() {
+		$('#mainContainer').load('components.html #addPackage', function(){
+			$('.dashTitle').text('DISCOVERY - PACKAGE NAME');
+			$('#pageIcon').text('T');
+			$('.formHeader').text('DISCOVERY - PACKAGE DETAILS');
+
+			$('#addPackageForm input, #addPackageForm select, #addPackageForm textarea').css('background', '#919191');
+
+			var $discoverySettings = $('#discoverySettings');
+
+			$('#addPackageForm').append($discoverySettings);
+		});
 	}
 }
 
