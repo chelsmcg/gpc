5f6ade2c4fca442d18b93994a1c85997398189ad
Chelsea <chelsea@sindesign.com.au>
2015-09-07 21:37:41 +1000

    schema

---
+++ sql/schema.sql
@@ -0,0 +1,60 @@
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS issues;
+DROP TABLE IF EXISTS files;
+DROP TABLE IF EXISTS packages;
+
+
+CREATE TABLE users (
+	id INT NOT NULL AUTO_INCREMENT,
+	email VARCHAR(90),
+	userName VARCHAR(200),
+	password VARCHAR(200),
+	fName VARCHAR(100),
+	lName VARCHAR(100),
+	hasLoggedin BOOLEAN DEFAULT 0,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE roles (
+	id INT(30),
+	type VARCHAR(90),
+	CONSTRAINT pk_roleId PRIMARY KEY (id, type),
+	FOREIGN KEY (id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+
+CREATE TABLE packages (
+	id INT NOT NULL AUTO_INCREMENT,
+	name VARCHAR(200),
+	type VARCHAR(10),
+	category VARCHAR(30),
+	priority VARCHAR(10),
+	status VARCHAR(20),
+	vendor VARCHAR(200),
+	version VARCHAR(20),
+	operatingSystem VARCHAR(100),
+	department VARCHAR(100),
+	revision VARCHAR(50),
+	comments TEXT,
+	added DATETIME,
+	PRIMARY KEY (id)
+);
+
+
+CREATE TABLE files (
+	id INT(30),
+	type VARCHAR(90),
+	name VARCHAR(100),
+	CONSTRAINT pk_filesId PRIMARY KEY (id, type),
+	FOREIGN KEY (id) REFERENCES packages(id) ON DELETE CASCADE
+);
+
+
+CREATE TABLE issues (
+	id INT NOT NULL AUTO_INCREMENT,
+	pId INT(30),
+	issue TEXT,
+	PRIMARY KEY (id),
+	FOREIGN KEY (pId) REFERENCES packages(id) ON DELETE CASCADE
+);
