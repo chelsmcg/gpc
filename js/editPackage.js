78ff07044d061652f0c330b0a326d5cc79cd68b9
Mark183 <mwganser@gmail.com>
2015-09-13 14:58:33 +1000

    added stage completions stuff

--- js/editPackage.js
+++ js/editPackage.js
@@ -1,119 +1,153 @@
 var EditPackage = {
 
-  packageData: [],
+	packageData: [],
 
 	init: function() {
 		EditPackage.events();
 	},
 
 	events: function() {
-    $('body').on('click touch', '#editPackageBtn', EditPackage.editDetails);
+		$('body').on('click touch', '#editPackageBtn', EditPackage.editDetails);
+		$('body').on('click touch', '#completeBtn', EditPackage.completedStage);
 	},
 
-  packageDetails: function(rowID) {
-    EditPackage.packageData = [];
-    $.ajax({
-        url: "php/getPackage.php",
-        data: {
-          rowId: rowID
-        },
-        dataType: 'jsonp',
-        success: function(response) {
-          console.log(response);
-          EditPackage.packageData = response.data.package[0]
-          var vendor = response.data.package[0].vendor;
-          var appID = response.data.package[0].appID;
-          var appName = response.data.package[0].name;;
-          var appVersion = response.data.package[0].version;
-          var opSystem = response.data.package[0].operatingSystem;
-          var packageType = response.data.package[0].type;
-          var revision = response.data.package[0].revision;
-          var priority = response.data.package[0].priority;
-          var comments = response.data.package[0].comments;
-          var status = response.data.package[0].status;
-          var packageName = Global.createPackagName(vendor, appName, appVersion, revision);
+	packageDetails: function(rowID) {
+		EditPackage.packageData = [];
+		$.ajax({
+			url: "php/getPackage.php",
+			data: {
+				rowId: rowID
+			},
+			dataType: 'jsonp',
+			success: function(response) {
+				console.log(response);
+				EditPackage.packageData = response.data.package[0]
+				var vendor = response.data.package[0].vendor;
+				var appID = response.data.package[0].appID;
+				var appName = response.data.package[0].name;;
+				var appVersion = response.data.package[0].version;
+				var opSystem = response.data.package[0].operatingSystem;
+				var packageType = response.data.package[0].type;
+				var revision = response.data.package[0].revision;
+				var priority = response.data.package[0].priority;
+				var comments = response.data.package[0].comments;
+				var status = response.data.package[0].status;
+				var packageName = Global.createPackagName(vendor, appName, appVersion, revision);
 
-          $('.dashTitle').text('EDIT PACKAGE - ' + packageName);
+				$('.dashTitle').text('EDIT PACKAGE - ' + packageName);
 
-          $('#appID').val(appID);
-          $('#vendor').val(vendor);
-          $('#appName').val(appName);
-          $('#appVersion').val(appVersion);
-          $('#opSystem').val(opSystem);
-          $('#packageType').val(packageType);
-          $('#revision').val(revision);
-          $('#priority').val(priority);
-          $('#comments').val(comments);
-          $('#status').val(status);
+				$('#appID').val(appID);
+				$('#vendor').val(vendor);
+				$('#appName').val(appName);
+				$('#appVersion').val(appVersion);
+				$('#opSystem').val(opSystem);
+				$('#packageType').val(packageType);
+				$('#revision').val(revision);
+				$('#priority').val(priority);
+				$('#comments').val(comments);
+				$('#status').val(status);
 
-        }
-    });
-  },
+			}
+		});
+	},
 
-  editDetails: function(){
-    var $this = $(this);
-    var vendor = $('#vendor').val();
-    var appID = $('#appID').val();
-    var appName = $('#appName').val();
-    var appVersion = $('#appVersion').val();
-    var opSystem = $('#opSystem').val();
-    var packageType = $('#packageType').val();
-    var revision = $('#revision').val();
-    var priority = $('#priority').val();
-    var comments = $('#comments').val();
+	editDetails: function(){
+		var vendor = $('#vendor').val();
+		var appID = $('#appID').val();
+		var appName = $('#appName').val();
+		var appVersion = $('#appVersion').val();
+		var opSystem = $('#opSystem').val();
+		var packageType = $('#packageType').val();
+		var revision = $('#revision').val();
+		var priority = $('#priority').val();
+		var comments = $('#comments').val();
 
 
-    $.ajax({
-        url: "php/editPackage.php",
-        data: {
-          rowId: EditPackage.packageData.rowID,
-          appId: appID,
-          vendor: vendor,
-          appName: appName,
-          appVersion: appVersion,
-          revision: revision,
-          OS: opSystem,
-          pType: packageType,
-          priority: priority,
-          comments: comments
-        },
-        dataType: 'jsonp',
-        success: function(response) {
-          console.log(response);
+		$.ajax({
+			url: "php/editPackage.php",
+			data: {
+				rowId: EditPackage.packageData.id,
+				appId: appID,
+				vendor: vendor,
+				appName: appName,
+				appVersion: appVersion,
+				revision: revision,
+				OS: opSystem,
+				pType: packageType,
+				priority: priority,
+				comments: comments,
+				type: 'edit'
+			},
+			dataType: 'jsonp',
+			success: function(response) {
+				console.log(response);
 
-          if(response.success){
-            AddPackage.successModal();
-          }else{
-            AddPackage.errorModal();
-          }
+				if(response.success){
+					AddPackage.successModal();
+				}else{
+					AddPackage.errorModal();
+				}
 
-        }
-    });
-  },
+			}
+		});
+	},
 
-  // completed: function(){
+	completedStage: function(){
 
-  //    $.ajax({
-  //       url: "php/editPackage.php",
-  //       data: {
-  //         rowId: EditPackage.packageData.rowID,
-          
-  //       },
-  //       dataType: 'jsonp',
-  //       success: function(response) {
-  //         console.log(response);
+		var status = $('#status').val();
+		var rowID = EditPackage.packageData.id;
+		var category = EditPackage.packageData.category;
+		var nextCategory = EditPackage.nextCategory(category);
+		var ajaxData = {
+			rowId: rowID,
+			status: status,
+			currentCategory: category,
+			nextCategory: nextCategory,
+			type: 'completedStage'
+		};
 
-  //         if(response.success){
-  //           AddPackage.successModal();
-  //         }else{
-  //           AddPackage.errorModal();
-  //         }
 
-  //       }
-  //   });
+		if(category == 'Discovery'){
+			ajaxData.sourceFile = 'file/path';
+			ajaxData.documentation = 'file/path';
+		}
 
-  // }
+		$.ajax({
+			url: "php/editPackage.php",
+			data: ajaxData,
+			dataType: 'jsonp',
+			success: function(response) {
+				console.log(response);
 
+				if(response.success){
+					AddPackage.successModal();
+				}else{
+					AddPackage.errorModal();
+				}
+
+			}
+		});
+	},
+
+	nextCategory: function(category){
+
+		var nextCategory;
+		if(category == 'Discovery'){
+			nextCategory = 'Packaging';
+
+		}else if(category == 'Packaging'){
+			nextCategory = 'Quality Assurance';
+
+		}else if(category == 'Quality Assurance'){
+			nextCategory = 'UAT';
+
+		}else if(category == 'UAT'){
+			nextCategory = 'Completed';
+
+		}
+
+		return nextCategory;
+	}
 }
 
 $(function() {
--- php/editPackage.php
+++ php/editPackage.php
@@ -5,7 +5,7 @@
 
 	// gp.dev/php/editPackage.php?rowId=7&vendor=checlc&appId=69&appName=funz&appVersion=3&revision=2.0
 
-	
+
 	if(!empty($_GET['rowId'])){
 		$rowId = $_GET['rowId'];
 
@@ -13,40 +13,140 @@
 		format_response(false, 'row id was not given', 'no_id');
 		die();
 	}
-		
-	$appId = !empty($_GET['appId']) ? $_GET['appId'] : null;
-	$vendor = !empty($_GET['vendor']) ? $_GET['vendor'] : null;
-	$appName = !empty($_GET['appName']) ? $_GET['appName'] : null;
-	$appVersion = !empty($_GET['appVersion']) ? $_GET['appVersion'] : null;
-	$revision = !empty($_GET['revision']) ? $_GET['revision'] : null;
-	$OS = !empty($_GET['OS']) ? $_GET['OS'] : null;
-	$pType = !empty($_GET['pType']) ? $_GET['pType'] : null;
-	$priority = !empty($_GET['priority']) ? $_GET['priority'] : null;
-	$comments = !empty($_GET['comments']) ? $_GET['comments'] : null;
-	$category = !empty($_GET['category']) ? $_GET['category'] : null;
-	$status = !empty($_GET['status']) ? $_GET['status'] : null;
-
-	// $added = date('Y-m-d H:i:s');
 
 
-	$packageUpdated = editPackage($rowId, $appId, $vendor, $appName, $appVersion, $revision, $OS, $pType, $priority, $comments, $category, $status);
+	//checks type exists. determine what type of edit is being requested
+	if(!empty($_GET['type'])){
 
-	if($packageUpdated){
+		//for a general edit
+		if($_GET['type'] == 'edit'){
+	
+			
+				
+			$appId = !empty($_GET['appId']) ? $_GET['appId'] : null;
+			$vendor = !empty($_GET['vendor']) ? $_GET['vendor'] : null;
+			$appName = !empty($_GET['appName']) ? $_GET['appName'] : null;
+			$appVersion = !empty($_GET['appVersion']) ? $_GET['appVersion'] : null;
+			$revision = !empty($_GET['revision']) ? $_GET['revision'] : null;
+			$OS = !empty($_GET['OS']) ? $_GET['OS'] : null;
+			$pType = !empty($_GET['pType']) ? $_GET['pType'] : null;
+			$priority = !empty($_GET['priority']) ? $_GET['priority'] : null;
+			$comments = !empty($_GET['comments']) ? $_GET['comments'] : null;
 
-		format_response($packageUpdated, 'Package updated successfully');
+			// $added = date('Y-m-d H:i:s');
+
+
+			$packageUpdated = editPackage($rowId, $appId, $vendor, $appName, $appVersion, $revision, $OS, $pType, $priority, $comments);
+
+			if($packageUpdated){
+
+				format_response($packageUpdated, 'Package updated successfully');
+
+			}else{
+				format_response(false, 'The package could not be updated', 'update_fail');
+			}
+
+
+
+		//for edit of package ofter a stage is complated
+		}else if($_GET['type'] == 'completedStage'){
+			
+			//checks all params exist
+			if(!empty($_GET['status']) && !empty($_GET['currentCategory']) && !empty($_GET['nextCategory'])){
+				
+				$status = $_GET['status'];
+				$currentCategory = $_GET['currentCategory'];
+				$nextCategory = $_GET['nextCategory'];
+
+				//checks to see if on discovery categpry
+				if($currentCategory != 'Discovery'){
+
+					$stageUpdated = updateStage($rowId, $status, $nextCategory);
+
+				
+				//when category is on discover then must also add files to package
+				}else if($currentCategory == 'Discovery' && !empty($_GET['sourceFile']) && !empty($_GET['documentation'])){
+
+					$sourceFile = $_GET['sourceFile'];
+					$documentation = $_GET['documentation'];
+
+					$stageUpdated = updateStage($rowId, $status, $nextCategory);
+
+					if($stageUpdated){
+
+						$filesUpdated = updateFiles($rowId, $sourceFile, $documentation);
+
+						if($filesUpdated){
+							format_response(true, 'stage and files added successfully');
+						}
+
+					}else{
+						format_response(false, 'The stage could not be updated', 'stageChange_fail');
+					}
+				}
+			}else{
+				format_response(false, 'missing params', 'stage_fail');
+			}
+		}
 
 	}else{
-		format_response(false, 'The package could not be updated', 'update_fail');
+		format_response(false, 'No type param recieved', 'update_fail');
 	}
 
 	
 
 	//updates the packages table
-	function editPackage($rowId, $appId, $vendor, $appName, $appVersion, $revision, $OS, $pType, $priority, $comments, $category, $status){
+	function editPackage($rowId, $appId, $vendor, $appName, $appVersion, $revision, $OS, $pType, $priority, $comments){
 		global $mysqli;
 
 		$sql = "UPDATE packages
 				SET appID='$appId', vendor='$vendor', name='$appName', version='$appVersion', revision='$revision', operatingSystem='$OS', type='$pType', priority='$priority', comments='$comments'
+				WHERE id='$rowId'";
+
+		if (!$result = $mysqli->query($sql)) {
+
+			printf("Errormessage 1 : %s\n", $mysqli->error);
+			return false;
+
+		} else {
+			if(mysqli_affected_rows($mysqli) != 0){
+				return true;
+			}
+
+			return false;
+		}
+
+	}
+
+	//updates category aand status
+	function  updateStage($rowId, $status, $nextCategory){
+		global $mysqli;
+
+		$sql = "UPDATE packages
+				SET status='$status', category='$nextCategory'
+				WHERE id='$rowId'";
+
+		if (!$result = $mysqli->query($sql)) {
+
+			printf("Errormessage 1 : %s\n", $mysqli->error);
+			return false;
+
+		} else {
+			if(mysqli_affected_rows($mysqli) != 0){
+				return true;
+			}
+
+			return false;
+		}
+
+	}
+
+	//updates files
+	function  updateFiles($rowId, $sourceFile, $documentation){
+		global $mysqli;
+
+		$sql = "UPDATE packages
+				SET docFile='$documentation', SourceFile='$sourceFile'
 				WHERE id='$rowId'";
 
 		if (!$result = $mysqli->query($sql)) {
