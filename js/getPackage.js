ef759f11058e9c13707049317a7001154bad5732
Chelsea <chelsea@sindesign.com.au>
2015-10-10 13:10:08 +1000

    added permissions for table icons

--- js/getPackage.js
+++ js/getPackage.js
@@ -11,107 +11,126 @@ var GetPackage = {
 	getAllPackages: function() {
 
 		$.ajax({
-  			url: "php/getPackage.php",
-  			dataType: 'jsonp',
-  			success: function(response) {
-  				console.log(response);
-          GetPackage.populateTable(response.data.package);
-  				GetPackage.userDetails(response.data.userData);
-  			}
+				url: "php/getPackage.php",
+				dataType: 'jsonp',
+				success: function(response) {
+					console.log(response);
+					GetPackage.populateTable(response.data.package);
+					GetPackage.userDetails(response.data.userData);
+					GetPackage.restrictUser();
+				}
 		});
 	},
 
-  populateTable: function(packages) {
-    var $this = $(this);
-    var package;
-    var vendor;
-    var appID;
-    var appName;
-    var appVersion;
-    var opSystem;
-    var packageType;
-    var revision;
-    var category;
-    var status;
-    var priority;
-    var comments;
-    var row_html;
-    var id;
-    var packageName;
+	restrictUser: function() {
+		$(".packageRow").each(function() {
+			var rowCategory = $(this).find('.packageCategory').text();
 
-    console.log(packages);
+		  	if( rowCategory != 'Discovery') {
+		  		$(this).find('.edit').addClass('disabled').removeClass('edit');
+		  	}
 
-    for(var i = 0; i < packages.length; i++) {
-      package = packages[i];
-      id = package.id;
-      vendor = package.vendor.replace(/\s+/g, '');
-      appID = package.appID;
-      appName = package.name.replace(/\s+/g, '');
-      appVersion = package.version.replace(/\s+/g, '');
-      opSystem = package.operatingSystem;
-      packageType = package.type;
-      revision = package.revision.replace(/\s+/g, '');
-      category = package.category;
-      status = package.status;
-      priority = package.priority;
-      comments = package.comments;
-      packageName = Global.createPackagName(vendor, appName, appVersion, revision);
+		  	if( rowCategory == 'Quality Assurance' && rowCategory == 'QAT' && rowCategory == 'Completed') {
+		  		$(this).find('.edit').addClass('disabled').removeClass('edit');
+		  		$(this).find('.issue').addClass('disabled').removeClass('issue');
+		  		$(this).find('.documents').addClass('disabled').removeClass('documents');
+		  		$(this).find('.source').addClass('disabled').removeClass('source');
+		  	}
 
-      if(category == 'Quality Assurance' || category == 'UAT' || category == 'Completed') {
-        row_html = '<tr data-rowid="' + id + '"><td>'+ appID +'</td><td class="packageName ' + category + 'PageBtn">'+ packageName + '<td>'+ packageType + '</td><td>' + priority + '</td><td>' + category + '</td><td>' + status + '</td><td class="tableIcon disabled">M</td><td class="tableIcon disabled">g</td><td class="tableIcon disabled">H</td><td class="tableIcon disabled">T</td></tr>';
-      }
-      else {
-        row_html = '<tr data-rowid="' + id + '"><td>'+ appID +'</td><td class="packageName ' + category + 'PageBtn">'+ packageName + '<td>'+ packageType + '</td><td>' + priority + '</td><td>' + category + '</td><td>' + status + '</td><td class="tableIcon edit">M</td><td class="tableIcon issue">g</td><td class="tableIcon documents">H</td><td class="tableIcon source">T</td></tr>';
-      }
+		  	if( Global.user.type != 'client' && Global.user.type != 'packager' && Global.user.type != 'c_tester' && Global.user.type != 'admin') {
+		  		$(this).find('.documents').addClass('disabled').removeClass('documents');
+		  		$(this).find('.source').addClass('disabled').removeClass('source');
+		  	}
+		});
+	},
 
-      $('#dashboardTable tbody').append(row_html);
-    }
+	populateTable: function(packages) {
+		var $this = $(this);
+		var package;
+		var vendor;
+		var appID;
+		var appName;
+		var appVersion;
+		var opSystem;
+		var packageType;
+		var revision;
+		var category;
+		var status;
+		var priority;
+		var comments;
+		var row_html;
+		var id;
+		var packageName;
+
+		console.log(packages);
+
+		for(var i = 0; i < packages.length; i++) {
+			package = packages[i];
+			id = package.id;
+			vendor = package.vendor.replace(/\s+/g, '');
+			appID = package.appID;
+			appName = package.name.replace(/\s+/g, '');
+			appVersion = package.version.replace(/\s+/g, '');
+			opSystem = package.operatingSystem;
+			packageType = package.type;
+			revision = package.revision.replace(/\s+/g, '');
+			category = package.category;
+			status = package.status;
+			priority = package.priority;
+			comments = package.comments;
+
+			packageName = Global.createPackageName(vendor, appName, appVersion, revision);
+
+			row_html = '<tr class="packageRow" data-rowid="' + id + '"><td class="packageID">'+ appID +'</td><td class="packageName ' + category + 'PageBtn">'+ packageName + '<td class="packageType">'+ packageType + '</td><td class="packagePriority">' + priority + '</td><td class="packageCategory">' + category + '</td><td class="packageStatus">' + status + '</td><td class="tableIcon edit">M</td><td class="tableIcon issue">g</td><td class="tableIcon documents">H</td><td class="tableIcon source">T</td></tr>';
+
+			$('#dashboardTable tbody').append(row_html);
+		}
 
 
-  },
+	},
 
-  userDetails: function(userData) {
-    var $this = $(this);
-    Global.user = userData;
-    console.log(userData);
-    var firstName = userData.fName;
-    var lastName = userData.lName;
+	userDetails: function(userData) {
+		var $this = $(this);
+		Global.user = userData;
+		console.log(userData);
+		var firstName = userData.fName;
+		var lastName = userData.lName;
 
-    var userName = firstName + ' ' + lastName;
+		var userName = firstName + ' ' + lastName;
 
-    $('.userName').text(userName);
-  }
+		$('.userName').text(userName);
+	}
 
-  // openUserDetails: function() {
-  //   console.log("yay");
-  //   var user_id = $(this).data('userid');
-  //   console.log(user_id);
-  //   $('.sidePanel').addClass('sidePanelOpen');
-  //   $.when(
-  //     $.ajax({
-  //       url: 'php/ajax_tutorial.php',
-  //       type: 'GET',
-  //       data: {
-  //         getUserDetails: true,
-  //         userId: user_id
-  //       },
-  //       dataType: 'jsonp'
-  //     })
-  //   ).done(function(response){
-  //     console.log(response);
-  //     var id = response.userId;
-  //     var name = response.name;
-  //     var email = response.email;
-  //     var age = response.age;
+	// openUserDetails: function() {
+	//   console.log("yay");
+	//   var user_id = $(this).data('userid');
+	//   console.log(user_id);
+	//   $('.sidePanel').addClass('sidePanelOpen');
+	//   $.when(
+	//     $.ajax({
+	//       url: 'php/ajax_tutorial.php',
+	//       type: 'GET',
+	//       data: {
+	//         getUserDetails: true,
+	//         userId: user_id
+	//       },
+	//       dataType: 'jsonp'
+	//     })
+	//   ).done(function(response){
+	//     console.log(response);
+	//     var id = response.userId;
+	//     var name = response.name;
+	//     var email = response.email;
+	//     var age = response.age;
 
-  //     $('#usersName').val(name);
-  //     $('#usersEmail').val(email);
-  //     $('#usersAge').val(age);
+	//     $('#usersName').val(name);
+	//     $('#usersEmail').val(email);
+	//     $('#usersAge').val(age);
 
-  //     // $('.sidePanel').data('userid', id);
-  //     $('.sidePanel').attr('data-userid', id);
-  //   });
-  // },
+	//     // $('.sidePanel').data('userid', id);
+	//     $('.sidePanel').attr('data-userid', id);
+	//   });
+	// },
 
 }
 
--- js/global.js
+++ js/global.js
@@ -10,7 +10,7 @@ var Global = {
 		$('body').on('touch click', '#cancelBtn', Load.dashboard)
 	},
 
-	createPackagName: function(vendor, appName, appVersion, revision){
+	createPackageName: function(vendor, appName, appVersion, revision){
 		var packageName = vendor + '_' + appName + '_' + appVersion + '_' + revision;
 		packageName = packageName.replace(/\s+/g, '');
 		return packageName;
