c30635545e40fa9e11c137c48e34294cc8d3c74c
Mark183 <mwganser@gmail.com>
2015-10-21 23:07:25 +1000

    set default server time to brisbane

--- php/commonFuncs.php
+++ php/commonFuncs.php
@@ -1,5 +1,6 @@
 <?php
 	session_start();
+	date_default_timezone_set('Australia/Brisbane');
 	
 	//formats data to be returned to ajax call
 	function format_response($success, $message=null, $data=null){
